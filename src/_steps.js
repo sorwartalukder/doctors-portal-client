546acf649287ee5602e0588b7f72363a5f4886cf
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-14 11:54:09 +0600

    first

---
+++ src/_steps.js
@@ -0,0 +1,11 @@
+/*
+1. create file
+npx create-react-app doctors-portal-client
+2.install tailwindCSS
+3. install daisyUI
+4. install react router dom
+4. create some folder and file
+5. create router
+
+*/
+
